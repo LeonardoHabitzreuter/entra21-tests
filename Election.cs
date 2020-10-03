089f90319665fddb0f3c9655f62e812a6c7be134
Entra21 <entra21@proway.treina>
2020-10-02 21:17:37 -0300

    ctor

--- Election.cs
+++ Election.cs
@@ -13,11 +13,21 @@ namespace entra21_tests
         // Propriedade pública SEMPRE em PascalCase
         // Propriedade apenas com GET pode ser usada com arrow
         public IReadOnlyCollection<(Guid id, string name, int votes)> Candidates => candidates;
-        
+     
+        public Election()
+        {
+            candidates = new List<(Guid id, string name, int votes)>();
+        }
+
         public bool CreateCandidates(List<string> candidateNames, string password)
         {
             if (password == "Pa$$w0rd")
             {
+                if (candidateNames == null)
+                {
+                    return true;
+                }
+                
                 candidates = candidateNames.Select(candidateName => {
                     return (Guid.NewGuid(), candidateName, 0);
                 }).ToList();
--- ElectionTest.cs
+++ ElectionTest.cs
@@ -17,7 +17,7 @@ namespace entra21_tests
             var created = election.CreateCandidates(candidates, "incorrect");
 
             // Deve / Asserções
-            Assert.Null(election.Candidates);
+            Assert.Empty(election.Candidates);
             Assert.False(created);
         }
 
